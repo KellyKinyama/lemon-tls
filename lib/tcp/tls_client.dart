a32501f0da97d3c9b70aff90e2a2fc8834453488
kelikovic <kkinyama@zesco.co.zm>
2026-04-09 14:10:01 +0200

    Update tls_client.dart

--- lib/tcp/tls_client.dart
+++ lib/tcp/tls_client.dart
@@ -1,28 +1,28 @@
-// import 'dart:io';
+import 'dart:io';
 
-// import 'package:lemon_tls/tls1_3.dart';
+import 'package:lemon_tls/tls1_3.dart';
 
-// void main() {
-//   Socket.connect('127.0.0.1', 4433).then((socket) {
-//     socket.listen((onData) {
-//       print("received: ${onData}");
-//     });
-//     // print("Data to send: $client_to_server");
-//     socket.write(client_to_server3);
-//     // socket.flush();
-//   });
-//   // final log = File('keylog.txt');
+void main() {
+  // Socket.connect('127.0.0.1', 4433).then((socket) {
+  //   socket.listen((onData) {
+  //     print("received: ${onData}");
+  //   });
+  //   // print("Data to send: $client_to_server");
+  //   socket.write(client_to_server3);
+  //   // socket.flush();
+  // });
+  final log = File('keylog.txt');
 
-//   // SecureSocket.connect(
-//   //   '127.0.0.1',
-//   //   4433,
-//   //   onBadCertificate: (certificate) => false,
-//   //   keyLog: (line) => log.writeAsStringSync(line, mode: FileMode.append),
-//   // ).then((socket) {
-//   //   socket.listen((onData) {
-//   //     print("received: ${onData}");
-//   //   });
-//   //   // print("Data to send: $client_to_server");
-//   //   // socket.write(client_to_server);
-//   // });
-// }
+  SecureSocket.connect(
+    '127.0.0.1',
+    8443,
+    onBadCertificate: (certificate) => true,
+    keyLog: (line) => log.writeAsStringSync(line, mode: FileMode.append),
+  ).then((socket) {
+    socket.listen((onData) {
+      print("received: ${onData}");
+    });
+    // print("Data to send: $client_to_server");
+    // socket.write(client_to_server);
+  });
+}
